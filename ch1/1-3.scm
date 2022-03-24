4047452e79bb17f8d2a21743b1efac8717c6f582
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-03-24 00:16:05 -0400

    1-4, 1-5

--- ch1/1-3.scm
+++ ch1/1-3.scm
@@ -1,3 +1,4 @@
+; Exercise 1.3.  Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 (define (square x) (* x x))
 (define (min a b c) (cond ((and (<= a b) (<= a c)) a) ((<= b c) b) (else c)))
 (define (ss a b c) (- (+ (square a) (square b) (square c)) (square (min a b c))))
